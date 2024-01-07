1f7b651e3b61f04def222097e73a6d1cb617cdc4
Mayank Sharma <82099885+codersharma2001@users.noreply.github.com>
2024-01-07 15:27:50 +0530

    Update Navbar.tsx

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -64,7 +64,7 @@ export function Navbar() {
                 modalTitleIconUrl="/images/navbar_icon.jpg"
                 switchToActiveChain={true}
                 auth={{
-                  loginOptional: true,  
+                  loginOptional: false,  
                 }}
               />
             </div>
