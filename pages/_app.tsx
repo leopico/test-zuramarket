f9413269078d3c1712f4f964522c1cec4c997521
Mayank Sharma <82099885+codersharma2001@users.noreply.github.com>
2024-01-16 20:13:37 +0530

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -22,11 +22,13 @@ function MyApp({ Component, pageProps }: AppProps) {
       clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
       activeChain={NETWORK}
       sdkOptions={{
-        gasless: false,
-        matic: {  // Use Matic network directly
-          rpcUrl: 'https://polygon-rpc.com',
-          chainId: 137,  // Mainnet chain ID for Matic
-        }, 
+        gasless: {
+          biconomy: {
+            apiId: process.env.NEXT_PUBLIC_BICONOMY_APIID,
+            apiKey: process.env.NEXT_PUBLIC_BICONOMY_APIKEY,
+            deadlineSeconds: 3600,
+          }
+        } 
       }}
       supportedWallets={[
         metamaskWallet(),
