40a79a1d9c00d5ac89a7b6e2c2d5ccaa35122bf8
leopico <linkoko.peceng@gmail.com>
2024-01-20 13:17:29 +0630

    tested

--- const/contractAddresses.ts
+++ const/contractAddresses.ts
@@ -2,8 +2,8 @@
 
 // 1. Set up the network your smart contracts are deployed to.
 // First, import the chain from the package, then set the NETWORK variable to the chain.
-import { Mumbai } from "@thirdweb-dev/chains";
-export const NETWORK = Mumbai;
+import { Polygon } from "@thirdweb-dev/chains";
+export const NETWORK = Polygon;
 
 // 2. The address of the marketplace V3 smart contract.
 // Deploy your own: https://thirdweb.com/thirdweb.eth/MarketplaceV3
@@ -11,7 +11,7 @@ export const MARKETPLACE_ADDRESS = "0xeBFc04743e8cbEb3778ff9C059DAADCE7610c550";
 
 // 3. The address of your NFT collection smart contract.
 export const NFT_COLLECTION_ADDRESS =
-  "0x40A5485DcD011531248d58D9f90A409b522a1729";
+  "0x4213E33A24BaD8dFb7F23c3cD6A6a2e164b0E034";
 
 //0x4213E33A24BaD8dFb7F23c3cD6A6a2e164b0E034
 //0x40A5485DcD011531248d58D9f90A409b522a1729
@@ -22,7 +22,7 @@ export const NFT_ZURAHOUSE_ADDRESS =
 
 // (Optional) Set up the URL of where users can view transactions on
 // For example, below, we use Mumbai.polygonscan to view transactions on the Mumbai testnet.
-export const ETHERSCAN_URL = "https://mumbai.polygonscan.com";
+export const ETHERSCAN_URL = "https://polygonscan.com";
 
 //https://mumbai.polygonscan.com
 //https://polygonscan.com
